54052c69f4656497b24dbc3d0fc9ec6f54c7cea7
Matej Šnuderl <matej.snuderl@cognite.com>
2020-01-25 17:24:44 +0100

    chore: bump dependencies (#24)

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.tsx'],
+  addons: ['@storybook/addon-storysource', '@storybook/addon-a11y'],
+};
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,9 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import camelCase from 'lodash.camelcase';
 import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
 import { terser } from 'rollup-plugin-terser';
 
 const pkg = require('./package.json');
@@ -20,25 +20,25 @@ export default {
       sourcemap: true,
       globals: {
         react: 'React',
-        'react-dom': 'ReactDOM'
-      }
+        'react-dom': 'ReactDOM',
+      },
     },
     {
       file: pkg.module,
       format: 'es',
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
-  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
   plugins: [
     // Allow json resolution
     json(),
     // Compile TypeScript files
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      typescript: require('typescript'),
+      objectHashIgnoreUnknownHack: true,
+    }),
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
     // Allow node_modules resolution, so you can use 'external' to control
@@ -52,13 +52,13 @@ export default {
       sourcemap: true,
       output: { comments: false },
       compress: {
-        pure_getters: true
+        pure_getters: true,
       },
       warnings: true,
       ecma: 5,
       // Compress and/or mangle variables in top level scope.
       // @see https://github.com/terser-js/terser
-      toplevel: true
-    })
-  ]
+      toplevel: true,
+    }),
+  ],
 };
